bc62b3836f54656bdbde43fcd4350ea5e2e9e9dc
Oluniyi <niyi@kairoscs.com>
2020-10-26 18:19:39 -0400

    Added this new file. Push to Git Test
    Added this new file. Push to Git Test

---
+++ CovidAPISeleniumScript.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class Testing123:
+    def testcase():
+        driver = webdriver.Chrome()
+        driver.get("https://covid19api.com")
+        print(driver.current_url)
+        driver.get_screenshot_as_file("COVID19APIChrome.png")
+        time.sleep(10)
+        driver.find_element(By.XPATH, "/html/body/div[1]/div/div/section[1]/div[2]/a/img").click()
+        time.sleep(10)
+        driver.get_screenshot_as_file("DigitalOceanChromeReferral.png")
+        driver.close()
+
+Testing123.testcase()
